33b9607a69db4a45d5887e665ed67fdc435bd38b
artem <sashheka@gmail.com>
2023-05-31 19:58:04 +0300

    made files and folders name refactoring, added ostrov_chistoty token

--- adv_statistics_mytarget_vk/ads_statistics_mytarget_vk.py
+++ adv_statistics_mytarget_vk/ads_statistics_mytarget_vk.py
@@ -40,37 +40,38 @@ params = {
     'metrics': 'all'
 }
 
-for account in accounts:
-    access_token = adv_tokens[account]['access_token']
+# for account in accounts:
+account = 'adv_ostrov_chistoty_mytarget'
+access_token = adv_tokens[account]['access_token']
 
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
+headers = {
+    'Authorization': f'Bearer {access_token}'
+}
 
-    response = requests.get(url, params=params, headers=headers)
-    data = response.json()
-    with open(f'assets/{account}.json', 'w') as f:
-        json.dump(data, f, indent=3)
+response = requests.get(url, params=params, headers=headers)
+data = response.json()
+with open(f'assets/{account}.json', 'w') as f:
+    json.dump(data, f, indent=3)
 
-    rows = data['items'][0]['rows']
-    df = pd.json_normalize(rows)
-    df = df.reset_index(drop=True)
-    df = df.rename(columns=lambda x: x.replace('.', '_'))
-    df.to_csv(f'assets/{account}.csv', index=False)
-    date_column_name = 'date'
+rows = data['items'][0]['rows']
+df = pd.json_normalize(rows)
+df = df.reset_index(drop=True)
+df = df.rename(columns=lambda x: x.replace('.', '_'))
+df.to_csv(f'assets/{account}.csv', index=False)
+date_column_name = 'date'
 
-    df = df.apply(lambda col: col.astype(data_type_mapper(col)) if col.name != date_column_name else col)
+df = df.apply(lambda col: col.astype(data_type_mapper(col)) if col.name != date_column_name else col)
 
-    engine = sqlalchemy.create_engine(mysql_engine)
-    inspector = sqlalchemy.inspect(engine)
-    table_name = account
+engine = sqlalchemy.create_engine(mysql_engine)
+inspector = sqlalchemy.inspect(engine)
+table_name = account
 
-    if not inspector.has_table(table_name):
-        df.head(0).to_sql(table_name, engine, if_exists='replace', index=False)
+if not inspector.has_table(table_name):
+    df.head(0).to_sql(table_name, engine, if_exists='replace', index=False)
 
-    df.to_sql(table_name, engine, if_exists='replace', index=False)
+df.to_sql(table_name, engine, if_exists='replace', index=False)
 
-    print(f"Data from {account} loaded.")
+print(f"Data from {account} loaded.")
 
 
 end_time = time.time()
