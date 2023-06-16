590e213b3c1d758a09ecbc7f97c5a0710d811296
artem <sashheka@gmail.com>
2023-06-16 12:10:01 +0300

    got members count from vk groups

---
+++ group_statistics_ok_vk/vk_members_count.py
@@ -0,0 +1,43 @@
+import datetime as dt
+import pandas as pd
+import requests
+import sqlalchemy
+import json
+from utils.utils import date_to_unix, unix_to_date, request
+
+
+with open('../resources/access_token.txt', 'r') as file:
+    access_token = file.read().strip()
+
+user_id = 689699902
+
+response = requests.get('https://api.vk.com/method/groups.get',
+                        params={
+                            'user_id': user_id,
+                            'access_token': access_token,
+                            'extended': 1,
+                            'filter': 'moder',
+                            'v': '5.131'
+                        }).json()
+
+groups = {}
+for item in response['response']['items']:
+    groups[item['id']] = item['screen_name']
+print(groups)
+
+groups_ids = [key for key, value in groups.items()]
+groups_ids_str = ', '.join(str(shortname) for shortname in groups_ids)
+
+response = requests.get('https://api.vk.com/method/groups.getById',
+                        params={
+                            'access_token': access_token,
+                            'group_ids': groups_ids_str,
+                            'fields': 'name, screen_name, members_count',
+                            'v': '5.131'
+                        }).json()
+
+response_for_df_test = response
+df = pd.DataFrame(response['response'])
+print(df)
+
+df.to_csv('assets_vk_members_count/member_counts.csv', index=False)
